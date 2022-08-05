4c883944ec2190e32c53ed4d91fcafafa25c8cc4
albertafriadii <albertafriadi01@gmail.com>
2022-08-05 09:23:54 +0700

    editing some function and add test for model

--- project-1/albertafriadii/migrations/001_create_table_users.sql
+++ project-1/albertafriadii/migrations/001_create_table_users.sql
@@ -5,5 +5,4 @@ CREATE TABLE users(
     password VARCHAR(100) NOT NULL, 
     created_at TIMESTAMP NOT NULL, 
     updated_at TIMESTAMP NOT NULL, 
-    deleted_at TIMESTAMP NOT NULL, 
     PRIMARY KEY(user_id));
